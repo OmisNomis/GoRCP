c35091f00862dc613f131634b761b0a88b51df8c
Si <simondrake1990@hotmail.co.uk>
2018-04-18 10:10:30 +0100

    Create main.go

---
+++ rpcServer/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"rpcprocs"
+)
+
+func main() {
+	arith := new(rpcprocs.Arith)
+	err := rpc.Register(arith)
+	if err != nil {
+		log.Fatalf("Format of service Arith isn't correct. %s", err)
+	}
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", ":1234")
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	log.Println("Serving RPC handler")
+	err = http.Serve(l, nil)
+	if err != nil {
+		log.Fatalf("Error serving: %s", err)
+	}
+}
